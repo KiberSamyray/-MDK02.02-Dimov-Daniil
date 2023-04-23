9fa82aa5c006f8487f16ae7de3af703eb833b4ec
Daniil <90115202+KiberSamyray@users.noreply.github.com>
2023-04-23 18:44:09 +0900

    Добавьте файлы проекта.

---
+++ MyPersonClass1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyPersonClass1
+{  
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        public string FirstName { get { return firstName; } set { firstName = value;  } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public int Age(DateTime Birthday)
+        {
+            int age = DateTime.Now.Year - Birthday.Year;
+            if(DateTime.Now.Month < Birthday.Month || DateTime.Now.Month == Birthday.Month && DateTime.Now.Day < Birthday.Day)
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
